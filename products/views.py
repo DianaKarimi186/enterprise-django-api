3e13f7c7bf427d9119a0fa4f12a00228cc7e6cbf
kayd123 <mbakadiana84@gmail.com>
2026-07-01 14:38:05 +0300

    perf: eradicate n+1 database bottleneck using select_related query optimization

--- products/views.py
+++ products/views.py
@@ -4,16 +4,15 @@ from .serializers import ProductSerializer
 from .tasks import simulate_heavy_background_job
 
 class ProductListCreateView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
+    # OPTIMIZATION FIX: select_related performs a SQL JOIN to pull categories instantly in 1 query!
+    queryset = Product.objects.select_related('category').all()
     serializer_class = ProductSerializer
 
     def perform_create(self, serializer):
         instance = serializer.save()
-
         simulate_heavy_background_job.delay(instance.name)
 
-
-
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
+    # OPTIMIZATION FIX: Optimized single item lookup queries
+    queryset = Product.objects.select_related('category').all()
     serializer_class = ProductSerializer
